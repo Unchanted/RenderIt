e6ffb6ab5228ace65ffbcddbed4480c2bb986295
Unchanted <omkkdwivedi@gmail.com>
2024-08-04 17:50:36 +0530

    refactor

--- src/main/java/com/arman/research /geom/transforms/MovingTransform3f.java
+++ src/main/java/com/arman/research /geom/transforms/MovingTransform3f.java
@@ -4,8 +4,7 @@ import com.arman.research.geom.vectors.Vector3f;
 
 public class MovingTransform3f extends Transform3f {
 
-    private static final Vector3f TEMP_VECTOR = new Vector3f();
-    private static final Vector3f ZERO_VECTOR = new Vector3f();
+    private static Vector3f temp = new Vector3f();
 
     private Vector3f velocity;
     private Movement velocityMovement;
@@ -14,16 +13,16 @@ public class MovingTransform3f extends Transform3f {
     private Movement velocityAngleZ;
 
     public MovingTransform3f() {
-        initialize();
+        velocity = new Vector3f(0, 0, 0);
+        velocityMovement = new Movement();
+        velocityAngleX = new Movement();
+        velocityAngleY = new Movement();
+        velocityAngleZ = new Movement();
     }
 
     public MovingTransform3f(Transform3f t) {
         super(t);
-        initialize();
-    }
-
-    private void initialize() {
-        velocity = new Vector3f();
+        velocity = new Vector3f(0, 0, 0);
         velocityMovement = new Movement();
         velocityAngleX = new Movement();
         velocityAngleY = new Movement();
@@ -31,48 +30,35 @@ public class MovingTransform3f extends Transform3f {
     }
 
     public void update(long elapsedTime) {
-        updatePosition(elapsedTime);
-        updateRotation(elapsedTime);
-    }
-
-    private void updatePosition(long elapsedTime) {
         float delta = velocityMovement.distance(elapsedTime);
         if (delta != 0) {
-            TEMP_VECTOR.setTo(velocity).multiply(delta);
-            getLocation().add(TEMP_VECTOR);
+            temp.setTo(velocity);
+            temp.multiply(delta);
+            getLocation().add(temp);
         }
-    }
-
-    private void updateRotation(long elapsedTime) {
-        rotateAngle(
-            velocityAngleX.distance(elapsedTime),
-            velocityAngleY.distance(elapsedTime),
-            velocityAngleZ.distance(elapsedTime)
-        );
+        rotateAngle(velocityAngleX.distance(elapsedTime), velocityAngleY.distance(elapsedTime), velocityAngleZ.distance(elapsedTime));
     }
 
     public void stop() {
-        velocity.setTo(ZERO_VECTOR);
-        resetMovement(velocityMovement);
-        resetMovement(velocityAngleX);
-        resetMovement(velocityAngleY);
-        resetMovement(velocityAngleZ);
-    }
-
-    private void resetMovement(Movement movement) {
-        movement.setTo(0, 0);
+        velocity.setTo(new Vector3f());
+        velocityMovement.setTo(0, 0);
+        velocityAngleX.setTo(0, 0);
+        velocityAngleY.setTo(0, 0);
+        velocityAngleZ.setTo(0, 0);
     }
 
     public void moveTo(Vector3f dest, float speed) {
-        TEMP_VECTOR.setTo(dest).subtract(getLocation());
-        float distance = TEMP_VECTOR.length();
-        long time = (long) (distance / speed);
-        TEMP_VECTOR.normalize().multiply(speed);
-        setVelocity(TEMP_VECTOR, time);
+        temp.setTo(dest);
+        temp.subtract(getLocation());
+        float dist = temp.length();
+        long time = (long) (dist / speed);
+        temp.divide(dist);
+        temp.multiply(speed);
+        setVelocity(temp, time);
     }
 
     public boolean isMoving() {
-        return !velocityMovement.stopped() && !velocity.equals(ZERO_VECTOR);
+        return !velocityMovement.stopped() && !velocity.equals(new Vector3f());
     }
 
     public boolean isMovingIgnoringY() {
@@ -80,7 +66,11 @@ public class MovingTransform3f extends Transform3f {
     }
 
     public long getRemainingMoveTime() {
-        return isMoving() ? velocityMovement.getRemainingTime() : 0;
+        if (!isMoving()) {
+            return 0;
+        } else {
+            return velocityMovement.getRemainingTime();
+        }
     }
 
     public Vector3f getVelocity() {
@@ -93,8 +83,8 @@ public class MovingTransform3f extends Transform3f {
 
     public void setVelocity(Vector3f v, long time) {
         velocity.setTo(v);
-        if (v.equals(ZERO_VECTOR)) {
-            resetMovement(velocityMovement);
+        if (v.equals(new Vector3f())) {
+            velocityMovement.setTo(0, 0);
         } else {
             velocityMovement.setTo(1, time);
         }
@@ -108,53 +98,139 @@ public class MovingTransform3f extends Transform3f {
         }
     }
 
-    public void rotateXTo(float targetAngle, float speed) {
-        rotateTo(velocityAngleX, getAngleX(), targetAngle, speed);
+    public void rotateXTo(float ad, float speed) {
+        rotateTo(velocityAngleX, getAngleX(), ad, speed);
     }
 
-    public void rotateYTo(float targetAngle, float speed) {
-        rotateTo(velocityAngleY, getAngleY(), targetAngle, speed);
+    public void rotateYTo(float ad, float speed) {
+        rotateTo(velocityAngleY, getAngleY(), ad, speed);
     }
 
-    public void rotateZTo(float targetAngle, float speed) {
-        rotateTo(velocityAngleZ, getAngleZ(), targetAngle, speed);
+    public void rotateZTo(float ad, float speed) {
+        rotateTo(velocityAngleZ, getAngleZ(), ad, speed);
     }
 
-    public void rotateXTo(float y, float z, float offsetAngle, float speed) {
-        rotateXTo((float) Math.atan2(-z, y) + offsetAngle, speed);
+    public void rotateXTo(float y, float z, float ao, float speed) {
+        rotateXTo((float) Math.atan2(-z, y) + ao, speed);
     }
 
-    public void rotateYTo(float x, float z, float offsetAngle, float speed) {
-        rotateYTo((float) Math.atan2(-z, x) + offsetAngle, speed);
+    public void rotateYTo(float x, float z, float ao, float speed) {
+        rotateYTo((float) Math.atan2(-z, x) + ao, speed);
     }
 
-    public void rotateZTo(float x, float y, float offsetAngle, float speed) {
-        rotateZTo((float) Math.atan2(y, x) + offsetAngle, speed);
+    public void rotateZTo(float x, float y, float ao, float speed) {
+        rotateZTo((float) Math.atan2(y, x) + ao, speed);
     }
 
-    private void rotateTo(Movement movement, float startAngle, float endAngle, float speed) {
-        if (startAngle == endAngle) {
+    public void rotateTo(Movement movement, float sa, float ea, float speed) {
+        // TODO
+        if (sa == ea) {
             movement.setTo(0, 0);
-            return;
-        }
-
-        float distanceClockwise = calculateDistanceClockwise(startAngle, endAngle);
-        float distanceCounterClockwise = calculateDistanceCounterClockwise(startAngle, endAngle);
-
-        if (distanceClockwise < distanceCounterClockwise) {
-            movement.setTo(-Math.abs(speed), (long) (distanceClockwise / -speed));
         } else {
-            movement.setTo(Math.abs(speed), (long) (distanceCounterClockwise / speed));
+            float distl;
+            float distr;
+            float pi2 = (float) (2 * Math.PI);
+            if (sa < ea) {
+                distl = sa - ea + pi2;
+                distr = ea - sa;
+            } else {
+                distl = sa - ea;
+                distr = ea - sa + pi2;
+            }
+            if (distl < distr) {
+                speed = -Math.abs(speed);
+                movement.setTo(speed, (long) (distl / -speed));
+            } else {
+                speed = Math.abs(speed);
+                movement.setTo(speed, (long) (distr / speed));
+            }
         }
     }
 
-    private float calculateDistanceClockwise(float startAngle, float endAngle) {
-        float pi2 = (float) (2 * Math.PI);
-        return startAngle < endAngle ? startAngle - endAngle + pi2 : startAngle - endAngle;
+    public void setVelocityAngleX(float speed) {
+        setVelocityAngleX(speed, -1);
     }
 
-    private float calculateDistanceCounterClockwise(float startAngle, float endAngle) {
-        float pi2 = (float) (2 * Math.PI);
-        return startAngle < endAngle ? endAngle - startAngle : endAngle - startAngle + pi2;
+    public void setVelocityAngleY(float speed) {
+        setVelocityAngleY(speed, -1);
     }
+
+    public void setVelocityAngleZ(float speed) {
+        setVelocityAngleZ(speed, -1);
+    }
+
+    public void setVelocityAngleX(float speed, long time) {
+        velocityAngleX.setTo(speed, time);
+    }
+
+    public void setVelocityAngleY(float speed, long time) {
+        velocityAngleY.setTo(speed, time);
+    }
+
+    public void setVelocityAngleZ(float speed, long time) {
+        velocityAngleZ.setTo(speed, time);
+    }
+
+    public float getVelocityAngleX() {
+        return isRotatingX() ? velocityAngleX.getSpeed() : 0;
+    }
+
+    public float getVelocityAngleY() {
+        return isRotatingY() ? velocityAngleY.getSpeed() : 0;
+    }
+
+    public float getVelocityAngleZ() {
+        return isRotatingZ() ? velocityAngleZ.getSpeed() : 0;
+    }
+
+    public boolean isRotatingX() {
+        return !velocityAngleX.stopped();
+    }
+
+    public boolean isRotatingY() {
+        return !velocityAngleY.stopped();
+    }
+
+    public boolean isRotatingZ() {
+        return !velocityAngleZ.stopped();
+    }
+
+    public Object clone() {
+        return new MovingTransform3f(this);
+    }
+
+    private static class Movement {
+
+        float speed;
+        long remainingTime;
+
+        public void setTo(float speed, long time) {
+            this.speed = speed;
+            this.remainingTime = time;
+        }
+
+        public boolean stopped() {
+            return (speed == 0) || (remainingTime == 0);
+        }
+
+        public float distance(long elapsedTime) {
+            if (remainingTime == 0) {
+                return 0;
+            } else if (remainingTime != -1) {
+                elapsedTime = Math.min(elapsedTime, remainingTime);
+                remainingTime -= elapsedTime;
+            }
+            return speed * elapsedTime;
+        }
+
+        public long getRemainingTime() {
+            return remainingTime;
+        }
+
+        public float getSpeed() {
+            return speed;
+        }
+
+    }
+
 }
